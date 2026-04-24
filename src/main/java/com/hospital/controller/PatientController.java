f2cbd1b234e3d63b638277609eef6e1e7d7e812d
krithik1204 <mukkeraraju1235@gmail.com>
2026-04-25 00:15:58 +0530

    Initial commit

---
+++ src/main/java/com/hospital/controller/PatientController.java
@@ -0,0 +1,17 @@
+package com.hospital.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+
+	@GetMapping("/health")
+	public String health() {
+		System.out.println("from health");
+		return "UP";
+	}
+
+}
