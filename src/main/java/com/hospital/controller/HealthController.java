2b031b53bbc83ac3ff9877a3241c6310761b2f20
krithik1204 <mukkeraraju1235@gmail.com>
2026-04-25 07:05:45 +0530

    Adding health controller

--- src/main/java/com/hospital/controller/HealthController.java
+++ src/main/java/com/hospital/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/patients")
-public class PatientController {
+public class HealthController {
 
 	@GetMapping("/health")
 	public String health() {
